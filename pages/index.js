6ba623cf1a5406a2a13d37e27ac6773af180e77f
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-08 20:00:20 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Layout from "../components/layout";
+import Heading from "../components/heading";
+
+const Index = () => {
+  return (
+    <>
+      <Heading title={"Multi-step Form"} />
+      <Layout>
+        <div className="centered-content fontFaceUbuntuRegular">
+          <div className="content-wrapper">
+            
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Index;
